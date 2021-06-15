4b39a08f02b4104b8ca89e49c08113db86ac1e1e
Task-hgq <85938216+Task-hgq@users.noreply.github.com>
2021-06-15 20:25:31 +0800

    Update hellowork.java

--- hellowork.java
+++ hellowork.java
@@ -1,5 +1,6 @@
+package hello;
 class hellowork{
-public static void main(String[]args){
-System.out.println("hellowork");
+      public static void main(String[]args){
+      System.out.println("hellowork!");
 }
 }
